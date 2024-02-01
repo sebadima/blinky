9e2c17dcf39f1406be8f471afbea8a089fa6be7a
Your Name <you@example.com>
2024-02-01 12:04:38 +0100

    fix

---
+++ src/main.ino
@@ -0,0 +1,17 @@
+#define LED 2
+
+void setup() {
+  // Setta la seriale a 115200 baud
+  Serial.begin(115200);
+  // Setta il pin 2 in modalità OUT
+  pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  delay(500);
+  digitalWrite(LED,HIGH);
+  Serial.println("Led è HIGH");
+  delay(500);
+  digitalWrite(LED,LOW);
+  Serial.println("Led è LOW");
+}
